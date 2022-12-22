5490c697670c2c3ba377685b9a2d6f0deeda67e8
Dário Souza <drio.srf@gmail.com>
2022-12-22 11:55:29 -0400

    Update questao006.c
    Completing the table with the four operations

--- Estrutura de controle e repetição I/questao006.c
+++ Estrutura de controle e repetição I/questao006.c
@@ -3,13 +3,16 @@
 
 int main(){
 	
-	for(int i=1; i<10; i++){
+	for(int i=1; i<11; i++){
 		
 		printf("Tabuada do %d\n", i);
 		
 		for(int j=1; j<11; j++){
 			
-			printf("%d x %d = %d\n", i, j, i*j);
+			printf("%d + %d = %d    ", i, j, i+j);
+			printf("%d - %d = %d    ", i, j, i-j);
+			printf("%d x %d = %d    ", i, j, i*j);
+			printf("%d / %d = %d\n", i*j, i, j*i/i);
 		}
 		
 		printf("\n");
